07c52ec6d2c92ae42c0d3c4c1ee02ce2fbb98981
zanterplan <zan.terplan@gmail.com>
2025-12-07 22:38:17 +0100

    CORS bypass for event controller

--- src/main/java/com/planify/eventmanager/controller/EventController.java
+++ src/main/java/com/planify/eventmanager/controller/EventController.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
 @Tag(name = "Events", description = "Event management operations")
